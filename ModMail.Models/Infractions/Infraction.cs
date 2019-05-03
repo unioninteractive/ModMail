80b8468917f5bc8351c763a70431d64450f5d7b3
Erlite <erlitedev@gmail.com>
2019-05-03 21:58:14 +0100

    Moderation.Infractions models

--- ModMail.Models/Infractions/Infraction.cs
+++ ModMail.Models/Infractions/Infraction.cs
@@ -8,31 +8,31 @@ namespace ModMail.Models
     [Table("Moderation.Infractions")]
     public class Infraction
     {
-        public long Id { get; set; }
+        public long Id { get; private set; }
 
         [Required]
-        public InfractionType Type { get; set; }
+        public InfractionType Type { get; private set; }
         
         [Required]
         [Column(TypeName = "timestamp with time zone")]
-        public DateTimeOffset Timestamp { get; set; }
+        public DateTimeOffset Timestamp { get; private set; }
         
         [Required]
-        public ulong StaffId { get; set; }
+        public ulong StaffId { get; private set; }
         
         [Required]
-        public ulong UserId { get; set; }
+        public ulong UserId { get; private set; }
         
-        public string Message { get; set; }
+        public string Reason { get; private set; }
 
         public Infraction(InfractionType type, DateTimeOffset timestamp, ulong staffId, ulong userId,
-            string message = null)
+            string reason = null)
         {
             Type = type;
             Timestamp = timestamp;
             StaffId = staffId;
             UserId = userId;
-            Message = message;
+            Reason = reason;
         }
     }
 }
